02be10c8d0a38a146c4cb7dabeb530f481340d64
Luca <52721603+lvcarx@users.noreply.github.com>
2024-09-27 18:49:17 +0200

    added digits example

---
+++ tests/digits_example.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.utils import shuffle
+from sklearn.preprocessing import MinMaxScaler
+from lsp.lsp import LSP
+
+if __name__ == '__main__':
+    digits = datasets.load_digits()
+    X = digits.data
+    y = digits.target
+    X, y = shuffle(X, y, random_state=0)
+    X = MinMaxScaler().fit_transform(X)
+
+    to_fit = X[:150]
+
+    lsp = LSP()
+    X_init = lsp.fit(to_fit)
+    X = lsp.transform(X_init, np.arange(0, 150), X)
+
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=3, cmap='tab10')
+    plt.axis('off')
+    plt.show()
